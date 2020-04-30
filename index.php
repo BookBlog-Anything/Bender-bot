cdb4dcc053fe000a918a97704a48ce1bb06dc7f2
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-04-30 17:01:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-echo 'laught out loud';
+$conn = "https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0/setwebhook?url=https://botbender-telegram.herokuapp.com/";
+$request = file_get_contents("php://input");
+$request = json_decode($request);
 ?>
