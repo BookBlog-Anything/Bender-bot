25217584617441dcd6169d4e5cc3b59da1ed6211
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-04-30 18:12:18 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  $conn_bot = "https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0";
+  $website = "https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0";
   $update = file_get_contents("php://input");
   $update =json_decode($update, true);
 
