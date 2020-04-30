820ae7cb63800b8d66e1c2f8f791382168cbcf79
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-04-30 17:33:40 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
   $conn_bot = "https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0";
-  $request = file_get_contents($request);
-  $request =json_decode($request);
+  $update = file_get_contents("php://input");
+  $update =json_decode($update, true);
 
+  $chatId = $update["Chat"],["Message"],["Id"];
+  $chatType = $update["Message"],["Chat"],["Type"];
 
+  switch(){
+    case "/ayuda" :
+      $response = "Aqui estoy que necesitas?";
+      break;
+  }
+
+  function sendmessage(){
+  
+  }
 ?>
