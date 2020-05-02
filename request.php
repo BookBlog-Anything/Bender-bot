b9d3b729c3acdde6c3ca9f166c41add5ec247235
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-02 17:13:56 -0400

    Update request.php

--- request.php
+++ request.php
@@ -1,3 +1,10 @@
 <?php
-  
+  if($message == "hola"){
+   $response = "dime que hay";
+    sendMessage($message, $response);
+  }
+  if($message == "Hola"){
+    $response = "dime que hay";
+    sendMessage($message, $response);
+  }
 ?>
