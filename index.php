6525ba1993194c7a9a8aea2ba785d1281de9a0d3
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-02 17:01:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-  $website = "https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0";
-  $update = file_get_contents("php://input");
-  $update =json_decode($update, TRUE);
-
-  $chatId = $update["message"]["chat"]["id"];
-  $chatType = $update["message"]["chat"]["type"];
-
-  $message = $update["message"]["text"];
-  $new_chat_members = $update["new_chat_members"];
+require 'conn.php';
   if($new_chat_members){
   	$responde = "Bienvenido al grupo";
 	sendMessage($new_chat_members, $response);
