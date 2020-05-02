9089f427ee786e6bcaea8189f81ffc9ed26c22e7
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-02 17:21:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
 <?php
-
 /* Conexion a la Api.telegram.org */
-require 'conn.php';
+$website = "https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0";
+  $update = file_get_contents("php://input");
+  $update =json_decode($update, TRUE);
+/*--------------------------------*/
+
+
+/*Enlance de datos*/
+  $chatId = $update["message"]["chat"]["id"];
+  $chatType = $update["message"]["chat"]["type"];
+
+  $message = $update["message"]["text"];
+  $new_chat_members = $update["new_chat_members"];
+
   if($new_chat_members){
   	$responde = "Bienvenido al grupo";
 	sendMessage($new_chat_members, $response);
   }
-
-/*---------------*/
-
+/*-------------------------------------------*/
 
 
 /* Requisito de datos  */
-require 'request.php';
+
+  if($message == "hola"){
+   $response = "dime que hay";
+    sendMessage($message, $response);
+  }
+  if($message == "Hola"){
+    $response = "dime que hay";
+    sendMessage($message, $response);
+  }
+/*------------------------------------*/
 
 /* Comandos programados */
   switch($message){
@@ -24,8 +42,7 @@ require 'request.php';
       $response = "Chupa el perro wey";
       sendMessage($chatId, $response);
   }
-
-
+/*---------------------------------------*/
 
 
 
@@ -34,6 +51,7 @@ require 'request.php';
     $url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId. '&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
   }
+/*-----------------------------------------------------------------*/
 ?>
 
 
