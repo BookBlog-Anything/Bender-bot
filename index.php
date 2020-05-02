bf6380fc3815f0ada281e666d24812b0ed6b5f7f
Alejandro Valdez <av0316702@gmail.com>
2020-05-02 16:50:02 -0400

    .

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       sendMessage($chatId, $response);
   }
 
-   if($message == "hola" OR "Hola"){
+   if($message == "hola","Hola"){
       $response = "Dime que hay";
      sendMessage($chatId, $response);
    }else{
