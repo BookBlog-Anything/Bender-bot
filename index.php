1ebec432c33934ee2de86ada0a1f8d3c439b46a4
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-03 16:40:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,20 +21,10 @@
       $response = "Chupa el perro wey";
       sendMessage($chatId, $response);
 	break;	  
-    case '/newpoll':
-   $poll = array(
-		'chat_id' => $chatId,
-		'options' => array('A','B','C'),
-		'question' => "Select that like you";
-		createPoll($chatId, $poll);
-   );
-      break;
   }
 
 
-	function createPoll($chatId, $poll){
- 		$url = $GLOBAL[website]. '/createPoll?chat_id$chatId&parse_mode=HTML&text='.urlencode($poll); 
-	}
+	
 /**/
 require 'request.php';
   
