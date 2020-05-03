ec54f2ad8e7cb6d0be0a7c074183a1a931e6ddf2
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-03 16:55:41 -0400

    Update request.php

--- request.php
+++ request.php
@@ -12,7 +12,7 @@
 
      }
 
- if(isset($message)) {
+ if(isset($update['message'])) {
    if(isset($update['message']['new_chat_member'])){
      $text = "hello";
     $url ="https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0/sendMessage?chat_id=". $chatId. "&text=" .$text;
