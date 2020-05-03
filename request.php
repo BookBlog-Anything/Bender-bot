9fc69197fcd8b890ca0b0fc992fd35b96e208e27
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-03 17:52:09 -0400

    Update request.php

--- request.php
+++ request.php
@@ -12,6 +12,11 @@
 
      }
 
-if($message == $update['username']. " has joined group"){
-sendMessage($chatId,"Welcome". $update['username']);}
+$options =  array("Devendra","noneofthese","Sourabh") ;
+
+$data = [ 'chat_id' => $chatId,   'question' => 'This is whose number 12345 ?',  'options' => json_encode($options)];
+
+$response = file_get_contents("https://api.telegram.org/botbot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0/sendPoll?" . http_build_query($data) );
+
+
 ?>
