1ed4d28fbb28b974fb0aa22bba766eea97e0c99a
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-03 17:05:53 -0400

    Update request.php

--- request.php
+++ request.php
@@ -12,11 +12,5 @@
 
      }
 
-/* if(isset($update['message'])) {
-   if(isset($update['message']['new_chat_member'])){
-     $text = "hello";
-    $url ="https://api.telegram.org/bot1274888263:AAEAMc678Wj6k1F5U_xV8kOuyXSuPOP1To0/sendMessage?chat_id=". $chatId. "&text=" .$text;
-    file_get_contents($url);
-   }
- }*/
+
 ?>
