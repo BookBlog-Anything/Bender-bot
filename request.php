51cbbcd509414c031c8b5aa8980b4cc85007e217
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-03 14:59:06 -0400

    Update request.php

--- request.php
+++ request.php
@@ -1,3 +1,15 @@
 <?php
-echo 'ss';
+ if($message == "hola"){
+      $response = "Dime que hay";
+     sendMessage($chatId, $response);
+   }else{
+   
+   }
+   if($message == "sabes quién es don miguelo"){
+     $response = "🥩Carne https://youtu.be/NjoZOYqgGwE";
+     sendMessage($chatId, $response);
+}else{
+     $response =" realmente no se";
+     sendMessage($chatId, $response);
+     }
 ?>
