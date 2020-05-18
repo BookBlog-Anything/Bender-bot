dcd31e9bfef8313c7ba514e11c5470cb5a745651
Alejandro Valdez <av0316702@gmail.com>
2020-05-18 17:50:50 -0400

    change of setting v.5

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type" : "text/plain"})
 	response.end("Hello World\n")
 }).listen(process.env.PORT)
+
+app.get('/', function(request, response) {
+	response.send("Lol")
+})
