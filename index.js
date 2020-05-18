e07b8529e381b786da2173fc6dba8b17baaa4470
Alejandro Valdez <av0316702@gmail.com>
2020-05-18 14:22:18 -0400

    change of setting 3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const port = process.env.PORT || 3000
 
 app.use(path.join(__dirname,'/src'))
 app.get('/', function (req, res ){
  res.send('Run')
 })
+
+app.listen(port)
