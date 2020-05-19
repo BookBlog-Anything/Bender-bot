1952e1861127132af685fc887f0fc23d8d0f793a
Alejandro Valdez <av0316702@gmail.com>
2020-05-18 20:25:55 -0400

    change of setting v.6.0

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var express = require('express')
 var app = express()
 var http = require('http')
 
-//http.createServer(function(req,res){
-	//res.writeHead(200, {"Content-Type" : "text/plain"})
-	//res.end("Hello")
-//})
-
 app.get("/", function(req, res){
 	res.send("Aqui siguo vivo")
 })
 
-http.listen(process.env.PORT)
+
+http.createServer(function(req,res){
+	res.writeHead(200, {"Content-Type" : "text/plain"})
+	res.end("Hello")
+}).listen(process.env.PORT)
+
+
+
 
