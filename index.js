3f2c951e8973519851b5bbf80209998366c54657
Alejandro Valdez <av0316702@gmail.com>
2020-05-18 20:56:29 -0400

    change of setting v.6.0

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send("Hello World"))
-app.get('webhooks/telegram', (req, res) => {
-	require(src/main.js)
+app.get('/', (req, res) => {
+res.send("Hello World")
+
 })
+
 app.listen(port, ()=> console.log("El servidor corre en el puerto ${port}"))
 
 
