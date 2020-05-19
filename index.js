15ea95096c3ddf2060fbf759584c4a3d187ee63e
Alejandro Valdez <av0316702@gmail.com>
2020-05-18 20:46:30 -0400

    change of setting v.6.0

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 var express = require('express')
 var app = express()
-var http = require('http')
 
+const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => res.send("Hello World"))
 
-
-http.createServer(function(req,res){
-	res.writeHead(200, {"Content-Type" : "text/plain"})
-	app.get("/setwebhook", function(req, res){
-	res.send("Aqui siguo vivo")
-})
-}).listen(process.env.PORT)
+app.listen(port, ()=> console.log("El servidor corre en el puerto ${port}"))
 
 
 
