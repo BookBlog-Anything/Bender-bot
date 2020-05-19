a63555c011fc6358588bfc616274ec2fb25a8419
Alejandro Valdez <av0316702@gmail.com>
2020-05-18 20:35:57 -0400

    change of setting v.6.0

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express')
 var app = express()
 var http = require('http')
 
-app.get("/setwebhook", function(req, res){
-	res.send("Aqui siguo vivo")
-})
+
 
 
 http.createServer(function(req,res){
 	res.writeHead(200, {"Content-Type" : "text/plain"})
-	res.end("Hello")
+	app.get("/setwebhook", function(req, res){
+	res.send("Aqui siguo vivo")
+})
 }).listen(process.env.PORT)
 
 
