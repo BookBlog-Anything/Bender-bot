b9657b76fc3a51324e967215dbe7026ccf10a019
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-22 15:44:48 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ bot.on('message',function(msg, match){
   const description =  "\nEsto es una comunidad de estudio de tecnologia en diferentes areas como:\nredes | seguridad | cloud | virtualizacion | servidores | VoIP,etc...\n\nEste es el Link para que se registren en la plataforma de Cisco Webex para futuras video conferencias: https://cart.webex.com/sign-up-webex \n\nSi tiene algun tipo de archivo o documento que quiera compartir porfavor conviertarlo en formato .rar y luego envielo. "
   const answer =["Redes","Seguridad","Cloud","Virtualizacion","Servidores","VoIP","Otros"]
 
-const chatId = msg.chat.id
+   const chatId = msg.chat.id
      const newUserName = msg.new_chat_member.username
      const newUserId = msg.new_chat_member.id
      const firstName = msg.new_chat_member.first_name
@@ -27,7 +27,7 @@ const chatId = msg.chat.id
      if(msg.new_chat_members){
          //bot.deleteMessage(msg.chat.id, messageId)
        bot.sendMessage(msg.chat.id, "¡Hola " + firstName + ", Bienvenido a la comunidad de " + msg.chat.title+ description )
-      bot.sendPoll(msg.chat.id,"Cual es tu preferencia de estudio",answer, opts)
+      bot.sendPoll(chatId,"Cual es tu preferencia de estudio",answer, opts)
 
     }
 
