c6b97428ab8234739f3e7a1adafc5bc550867b77
Valdez Alejandro <40470173+v2414@users.noreply.github.com>
2020-05-21 21:43:42 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,16 +10,16 @@ const bot = new Telegraf(process.env.TELEGRAM_API_TOKEN ,{polling:true})
   //
 bot.on('message',function(msg, match){
   console.log(msg);
-  var description =  "este es una comunidad de estudio de tecnologia en diferentes areas como:\n redes | seguridad | cloud | virtualizacion | servidores | VoIP,etc...\n \n Este es el Link para que se registren en la plataforma de Cisco Webex para futuras video conferencias: https://cart.webex.com/sign-up-webex \n \n  si tiene algun tipo de archivo o documento que quiera compartir porfavor conviertarlo en formato .rar y luego envielo. "
-  var options =["Redes","Seguridad","Cloud","Virtualizacion","Servidores","VoIP","Otros"]
-  var example
-  var chatId = msg.chat.id
-     var newUserName = msg.new_chat_member.username
-     var newUserId = msg.new_chat_member.id
-     var firstName = msg.new_chat_member.first_name
-     var messageId = msg.message_id
-     var extra ={
-       'is_anonymous': true,
+  const description =  "este es una comunidad de estudio de tecnologia en diferentes areas como:\n redes | seguridad | cloud | virtualizacion | servidores | VoIP,etc...\n \n Este es el Link para que se registren en la plataforma de Cisco Webex para futuras video conferencias: https://cart.webex.com/sign-up-webex \n \n  si tiene algun tipo de archivo o documento que quiera compartir porfavor conviertarlo en formato .rar y luego envielo. "
+  const answer =["Redes","Seguridad","Cloud","Virtualizacion","Servidores","VoIP","Otros"]
+
+const chatId = msg.chat.id
+     const newUserName = msg.new_chat_member.username
+     const newUserId = msg.new_chat_member.id
+     const firstName = msg.new_chat_member.first_name
+     const messageId = msg.message_id
+     const opts ={
+       'is_anonymous': false,
         'allows_multiple_answers': true
         }
 
@@ -27,7 +27,7 @@ bot.on('message',function(msg, match){
      if(msg.new_chat_members){
          //bot.deleteMessage(msg.chat.id, messageId)
        bot.sendMessage(msg.chat.id, "¡Hola " + firstName + ", Bienvenido a la comunidad de Technology Study " + msg.chat.title+ description )
-      bot.sendPoll(msg.chat.id,"Cual es tu preferencia de estudio",options,extra)
+      bot.sendPoll(msg.chat.id,"Cual es tu preferencia de estudio",answer, opts)
 
     }
 
